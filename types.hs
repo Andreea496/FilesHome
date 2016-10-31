0f124b4448655e6fef30c415cd34892b12395b87
Andreea496 <andreea.stanciu4796@yahoo.com>
2016-10-31 20:02:11 +0000

    Add files via upload

---
+++ types.hs
@@ -0,0 +1,16 @@
+data List a = Nil | Cons a (List a)
+
+hd :: List a -> a
+hd Nil = error "Nil does not have a had"
+hd (Cons x _) = x
+
+tl :: List a -> List a
+tl Nil = error "Nil does not have a tail"
+tl (Cons _ l) = l
+
+data Btree a = Leaf a | L (Btree a) a | R a (Btree a) | LR (Btree a) a (Btree a)
+
+addNat :: Nat -> Nat -> Nat
+addNat n Zero = n
+addNat n (Succ m) = Succ (add n m) 
+
