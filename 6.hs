1d1876cce6343c00d9e4e94d39212fafaa8fefc9
Andreea496 <andreea.stanciu4796@yahoo.com>
2016-10-31 23:40:54 +0000

    Add files via upload

--- 6.hs
+++ 6.hs
@@ -14,11 +14,11 @@ addDate :: Int -> Int -> Int -> Date
 addDate d m y | d < 0 || d > 31 = error "Inavlid day"
 			  | m `notElem` [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12] = error "Invalid month"
 		  	  | d > 30 && m `elem` [02, 04, 06, 09, 11] = error "Invalid day for this month"
-      		  | d > 29 && m == 02 = error "Invalid day for this month" -- still a need to check for leap years
-			  | y < 1000 || y > 9999 = error "Invalid year"
+      		  | d > 29 && m == 02 && (leapCheck y == True) = error "Invalid day for this month in leap year" 
+      		  | d > 28 && m == 02 && (leapCheck y == False) = error "Invalid day for this month" 
+			  | y < 0 = error "Invalid year"
 			  | otherwise = Date d m y
-
-
+			  
 showd :: Date -> String
 
 showd d = "Date: " ++ show(day d) ++ "/" ++ show(month d) ++ "/" ++ show(year d)
