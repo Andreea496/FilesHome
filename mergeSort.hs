8ff07fd5a75e1a22fb6a974cebc65784b12041cf
Andreea496 <andreea.stanciu4796@yahoo.com>
2016-12-03 16:52:31 +0000

    Delete mergeSort.hs

--- mergeSort.hs
+++
@@ -1,5 +0,0 @@
- msort :: (Ord a) => [a] -> [a] -> [a]
- msort xs [] = xs
- msort [] ys = ys
- msort (x:xs) (y:ys) | (x <= y)  = x:(msort xs (y:ys)) 
-          		 	 | otherwise = y:(msort (x:xs) ys)
